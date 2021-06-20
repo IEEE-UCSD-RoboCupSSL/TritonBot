529c92ab86c866973c230679b45fec7bf9344731
zht <hoz043@ucsd.edu>
2021-06-20 02:41:50 -0700

    bug fix

--- source/CoreModules/DataCmdTypes.cpp
+++ source/CoreModules/DataCmdTypes.cpp
@@ -22,7 +22,7 @@ BotData defaultBotData() {
     data.vel = {0, 0};
     data.ang = 0.0f;
     data.angVel = 0.0f;
-    data.frame = NotDetermined;
+    data.frame = BodyFrame;
     return data;
 }
 
@@ -30,7 +30,7 @@ BallData defaultBallData() {
     BallData data;
     data.pos = {0, 0};
     data.vel = {0, 0};
-    data.frame = NotDetermined;
+    data.frame = BodyFrame;
     return data;
 }
 
